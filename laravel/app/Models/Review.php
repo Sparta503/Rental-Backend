e96ca6581e317c021ea75dfb0f1721b01b461ce7
Sparta503 <chomunodisaa68@gmail.com>
2026-05-12 20:41:39 +0200

    Update Review.php

--- laravel/app/Models/Review.php
+++ laravel/app/Models/Review.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'item_id',
+        'rating',
+        'comment'
+    ];
+
+    protected $casts = [
+        'rating' => 'integer'
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function item()
+    {
+        return $this->belongsTo(Item::class);
+    }
 }
